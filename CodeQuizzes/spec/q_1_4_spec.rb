2e5f554f780a1c5cc63a2cdf86918b870d2e6b4c
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-06 18:07:11 +0100

    Added test for Q4 in T1

---
+++ CodeQuizzes/spec/q_1_4_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require '../lib/q1_4'
+
+describe "#vowels" do
+  it "returns array of words with vowels" do
+    str = "24 hour roadside resistance"
+    expected = %w|hour roadside resistance|
+    expect(vowels(str)).to eq expected
+  end
+end
