66ad100b3ee5c29cc564ef2bc9b7ab6d5f2a1eca
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-06 20:17:39 +0100

    Added test for Q8 in T1

---
+++ CodeQuizzes/spec/q_1_8_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require '../lib/q1_6'
+
+describe "#delete_last" do
+  it "removes last n elements from string" do
+    str = "hello"
+    expect(str.delete_last(2)).to eq "hel"
+  end
+
+  it "mutates self in place" do
+    str = "blah"
+    expect(str.delete_last(1)).to equal str.replace "bla"
+  end
+end
