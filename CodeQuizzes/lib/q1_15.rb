2e1512fdf877a959cd2de13698d3f85ae699791e
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-06 20:42:01 +0100

    Edited test for Q15 in T1 & added implementation

---
+++ CodeQuizzes/lib/q1_15.rb
@@ -0,0 +1,5 @@
+class Integer
+  def concat(number)
+    (self.to_s + number.to_s).to_i
+  end
+end
---
+++ CodeQuizzes/spec/q_1_15_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require '../lib/q1_15'
+
+describe "#concat" do
+  it "concatenates two integers" do
+    expect(42.concat(99)).to eq 4299
+  end
+end
