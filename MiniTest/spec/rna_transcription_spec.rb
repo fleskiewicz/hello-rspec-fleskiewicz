2413505f4f3a47e2758fa78869e78b4a0d9bd929
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-07 02:35:51 +0100

    Added RSpec test for RNA transcription exercism

---
+++ MiniTest/spec/rna_transcription_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require_relative '../lib/rna_transcription'
+
+describe "#of_dna" do
+  it "should return guanine from cytosine" do
+    expect(Complement.of_dna("C")).to eql("G")
+  end
+
+  it "should return cytosine from guanine" do
+    expect(Complement.of_dna("G")).to eql("C")
+  end
+
+  it "should return adenine from thymine" do
+    expect(Complement.of_dna("T")).to eql("A")
+  end
+
+  it "should return uracil from adenine" do
+    expect(Complement.of_dna("A")).to eql("U")
+  end
+
+  it "should return rna complement" do
+    expect(Complement.of_dna("ACGTGGTCTTAA")).to eql("UGCACCAGAAUU")
+  end
+
+  it "should correctly handle invalit input rna instead of dna" do
+    expect(Complement.of_dna("U")).to eql('')
+  end
+
+  it "should correctly handle completely invalid dna input" do
+    expect(Complement.of_dna("XXX")).to eql('')
+  end
+
+  it "should correctly handle partially invalid dna input" do
+    expect(Complement.of_dna("ACGTXXXCTTAA")).to eql('')
+  end
+
+end
