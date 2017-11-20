27a7999a7e17dde63a649eb76cae711a62e2f13c
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-20 19:39:09 +0100

    Added test for decimal numbers

--- InsertionSort/spec/insertion_spec.rb
+++ InsertionSort/spec/insertion_spec.rb
@@ -16,4 +16,14 @@ describe "#sort" do
     array = [3, 44, 567, 1, 5, 31, 67]
     expect(sort(array)).to eq([1, 3, 5, 31, 44, 67, 567])
   end
+
+  it "should proper sort negative decimals" do
+    array = [-9.394, -9.393, -9.19, -1.4956, -1.4954, -23.3, -111.111]
+    expect(sort(array)).to eq([-111.111, -23.3, -9.394, -9.393, -9.19, -1.4956, -1.4954])
+  end
+
+  it "should proper sort positive decimals" do
+    array = [3.9455, 2.12, 2.02, 9.346, 0.312, 934.40, 9583.11, 444.111, 9584.345]
+    expect(sort(array)).to eq([0.312, 2.02, 2.12, 3.9455, 9.346, 444.111, 934.40, 9583.11, 9584.345])
+  end
 end
