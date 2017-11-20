f206c786b6f1a155552bf94ff61214bdcac2bffd
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-20 19:34:44 +0100

    Added tests for integers

--- InsertionSort/spec/insertion_spec.rb
+++ InsertionSort/spec/insertion_spec.rb
@@ -6,4 +6,14 @@ describe "#sort" do
     array = []
     expect(sort(array)).to eq([])
   end
+
+  it "should proper sort negative integers" do
+    array = [-3, -4, -1, -99, -7, -13]
+    expect(sort(array)).to eq([-99, -13, -7, -4, -3, -1])
+  end
+
+  it "should proper sort positive integers" do
+    array = [3, 44, 567, 1, 5, 31, 67]
+    expect(sort(array)).to eq([1, 3, 5, 31, 44, 67, 567])
+  end
 end
