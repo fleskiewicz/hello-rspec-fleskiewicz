ac2e08fa65740e9d56f3884bdc4ab38899098c54
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-20 19:21:46 +0100

    Implemented insertion sort algorithm, proceeding to tests

--- InsertionSort/lib/insertion.rb
+++ InsertionSort/lib/insertion.rb
@@ -1,11 +1,11 @@
 def sort(array)
-n = array.length - 1
-  n.times do
-    time_to_do_things.times do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
-      end
-      array
+  0..array.length.times do |i|
+    item = array[i]
+    while item < array[i - 1] && i > 0
+      array[i] = array[i - 1]
+      array[i - 1] = item
+      i -= 1
     end
   end
+  array
 end
