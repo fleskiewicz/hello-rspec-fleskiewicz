e78658b16cf847a3c31426c1c5257ae61559772a
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-20 20:06:57 +0100

    Extended insertion sort with ascending/descending mode and adjusted tests

--- InsertionSort/lib/insertion.rb
+++ InsertionSort/lib/insertion.rb
@@ -1,10 +1,21 @@
-def sort(array)
-  0..array.length.times do |i|
-    item = array[i]
-    while item < array[i - 1] && i > 0
-      array[i] = array[i - 1]
-      array[i - 1] = item
-      i -= 1
+def sort(array, mode)
+  if mode == 'asc'
+    0..array.length.times do |i|
+      item = array[i]
+      while item < array[i - 1] && i > 0
+        array[i] = array[i - 1]
+        array[i - 1] = item
+        i -= 1
+      end
+    end
+  elsif mode == 'desc'
+    0..array.length.times do |i|
+      item = array[i]
+      while item > array[i - 1] && i > 0
+        array[i] = array[i - 1]
+        array[i - 1] = item
+        i -= 1
+      end
     end
   end
   array
--- InsertionSort/spec/insertion_spec.rb
+++ InsertionSort/spec/insertion_spec.rb
@@ -2,43 +2,43 @@ require 'rspec'
 require_relative '../lib/insertion'
 
 describe "#sort" do
-  it "should return empty array when trying to sort it" do
+  it "should return empty array when trying to ascending sort it" do
     array = []
-    expect(sort(array)).to eq([])
+    expect(sort(array, 'asc')).to eq([])
   end
 
-  it "should proper sort negative integers" do
+  it "should proper ascending sort negative integers" do
     array = [-3, -4, -1, -99, -7, -13]
-    expect(sort(array)).to eq([-99, -13, -7, -4, -3, -1])
+    expect(sort(array, 'asc')).to eq([-99, -13, -7, -4, -3, -1])
   end
 
-  it "should proper sort positive integers" do
+  it "should proper ascending sort positive integers" do
     array = [3, 44, 567, 1, 5, 31, 67]
-    expect(sort(array)).to eq([1, 3, 5, 31, 44, 67, 567])
+    expect(sort(array, 'asc')).to eq([1, 3, 5, 31, 44, 67, 567])
   end
 
-  it "should proper sort negative decimals" do
+  it "should proper ascending sort negative decimals" do
     array = [-9.394, -9.393, -9.19, -1.4956, -1.4954, -23.3, -111.111]
-    expect(sort(array)).to eq([-111.111, -23.3, -9.394, -9.393, -9.19, -1.4956, -1.4954])
+    expect(sort(array, 'asc')).to eq([-111.111, -23.3, -9.394, -9.393, -9.19, -1.4956, -1.4954])
   end
 
-  it "should proper sort positive decimals" do
+  it "should proper ascending sort positive decimals" do
     array = [3.9455, 2.12, 2.02, 9.346, 0.312, 934.40, 9583.11, 444.111, 9584.345]
-    expect(sort(array)).to eq([0.312, 2.02, 2.12, 3.9455, 9.346, 444.111, 934.40, 9583.11, 9584.345])
+    expect(sort(array, 'asc')).to eq([0.312, 2.02, 2.12, 3.9455, 9.346, 444.111, 934.40, 9583.11, 9584.345])
   end
 
-  it "should return one element array when trying to sort it" do
+  it "should return one element array when trying to ascending sort it" do
     array = [3]
-    expect(sort(array)).to eq([3])
+    expect(sort(array, 'asc')).to eq([3])
   end
 
-  it "should proper sort two positive integers" do
+  it "should proper ascending sort two positive integers" do
     array = [5, 3]
-    expect(sort(array)).to eq([3, 5])
+    expect(sort(array, 'asc')).to eq([3, 5])
   end
 
-  it "should propert sort two negative integers" do
+  it "should propert ascending sort two negative integers" do
     array = [-22, -57]
-    expect(sort(array)).to eq([-57, -22])
+    expect(sort(array, 'asc')).to eq([-57, -22])
   end
 end
