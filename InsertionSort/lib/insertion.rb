a37deca5b08796404c8841356c7f58e7847ed66a
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-20 22:11:16 +0100

    Made two seperate methods for asc/desc sorting

--- InsertionSort/lib/insertion.rb
+++ InsertionSort/lib/insertion.rb
@@ -1,22 +1,26 @@
-def sort(array, mode)
-  if mode == 'asc'
-    0..array.length.times do |i|
-      item = array[i]
-      while item < array[i - 1] && i > 0
-        array[i] = array[i - 1]
-        array[i - 1] = item
-        i -= 1
-      end
-    end
-  elsif mode == 'desc'
-    0..array.length.times do |i|
-      item = array[i]
-      while item > array[i - 1] && i > 0
-        array[i] = array[i - 1]
-        array[i - 1] = item
-        i -= 1
-      end
+def sort_asc(array)
+  0..array.length.times do |i|
+    item = array[i]
+    while item < array[i - 1] && i > 0
+      swap(array, item, i)
+      i -= 1
     end
   end
   array
+end
+
+def sort_desc(array)
+  0..array.length.times do |i|
+    item = array[i]
+    while item > array[i - 1] && i > 0
+      swap(array, item, i)
+      i -= 1
+    end
+  end
+  array
+end
+
+def swap(array, item, i)
+  array[i] = array[i - 1]
+  array[i - 1] = item
 end
