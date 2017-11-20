07d009d698a1e11a3d1a1555b3107f6f33605ce6
fleskiewicz <filipleskiewicz@gmail.com>
2017-11-20 19:45:08 +0100

    Added some unusual cases tests

--- InsertionSort/spec/insertion_spec.rb
+++ InsertionSort/spec/insertion_spec.rb
@@ -26,4 +26,19 @@ describe "#sort" do
     array = [3.9455, 2.12, 2.02, 9.346, 0.312, 934.40, 9583.11, 444.111, 9584.345]
     expect(sort(array)).to eq([0.312, 2.02, 2.12, 3.9455, 9.346, 444.111, 934.40, 9583.11, 9584.345])
   end
+
+  it "should return one element array when trying to sort it" do
+    array = [3]
+    expect(sort(array)).to eq([3])
+  end
+
+  it "should proper sort two positive integers" do
+    array = [5, 3]
+    expect(sort(array)).to eq([3, 5])
+  end
+
+  it "should propert sort two negative integers" do
+    array = [-22, -57]
+    expect(sort(array)).to eq([-57, -22])
+  end
 end
